8b227435debe506b39ec318d6f80b8ad3422027a
ProjectDunk05 <green.adam10@sky.com>
2024-03-06 15:33:01 +0700

    add algorithm in pseudocode

---
+++ script.js
@@ -0,0 +1,7 @@
+//When a user inputs "Rock", "Paper" or "Scissors"
+//Generate a random computers selection of "Rock","Paper" or "Scissors"
+//If the user input is "Rock" and the computers selection is "Scissors" show "You Win!"
+//If the user input is "Rock" and the computers selection is "Paper" show "You Lose!"
+//If the user input is "Rock" and the computers selection is "Rock" show "It's A Draw!"
+// Repeat the above 3 steps for user input of "Scissors" and "Paper"
+// When finished add button to play again

19c9df78b1d78d12a00be891adc2caea2e4c870d
ProjectDunk05 <green.adam10@sky.com>
2024-03-15 15:26:43 +0700

    add getComputerChoice function that assigns 0,1 or 2 to Rock, Paper or Scissors

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 //When a user inputs "Rock", "Paper" or "Scissors"
 //Generate a random computers selection of "Rock","Paper" or "Scissors"
-
-    function getRandomInteger() {
-        let num = 3
-        return Math.floor(Math.random() * num);
+ 
+   function getRandomInteger() {
+        return Math.floor(Math.random() * 3);
     };
+    function getComputerChoice() {
+        if (getRandomInteger() == 0) {
+            return "Rock";
+        }  else if (getRandomInteger() == 1) {
+            return "Paper";
+        }  else {
+            return "Scissors"
+        }
+    };
+    
+
+   
 //If the user input is "Rock" and the computers selection is "Scissors" show "You Win!"
 //If the user input is "Rock" and the computers selection is "Paper" show "You Lose!"
 //If the user input is "Rock" and the computers selection is "Rock" show "It's A Draw!"

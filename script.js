c9bf5bb3facf7f22be25574b3110afb4af6bc2b6
ProjectDunk05 <green.adam10@sky.com>
2024-03-15 09:04:32 +0700

    generate random number (0,1,2)

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //When a user inputs "Rock", "Paper" or "Scissors"
 //Generate a random computers selection of "Rock","Paper" or "Scissors"
+
+    function getRandomInteger() {
+        let num = 3
+        return Math.floor(Math.random() * num);
+    };
 //If the user input is "Rock" and the computers selection is "Scissors" show "You Win!"
 //If the user input is "Rock" and the computers selection is "Paper" show "You Lose!"
 //If the user input is "Rock" and the computers selection is "Rock" show "It's A Draw!"

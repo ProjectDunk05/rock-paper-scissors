fc8429005ea1c4ab8fa90401c4900837bda33cb5
ProjectDunk05 <green.adam10@sky.com>
2024-03-19 15:19:06 +0700

    add function to produce a case insensitive player selection

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 //When a user inputs "Rock", "Paper" or "Scissors"
+
+    let userInput = prompt("Please enter Rock, Paper or Scissors");
+   
+    function capitalise(str) {
+        return ((str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()));
+    };
+
+    function playerSelection() {
+        return "You selected" + " " + capitalise(userInput);
+    };
+
 //Generate a random computers selection of "Rock","Paper" or "Scissors"
  
    function getRandomInteger() {
@@ -6,15 +17,14 @@
     };
     function getComputerChoice() {
         if (getRandomInteger() == 0) {
-            return "Rock";
-        }  else if (getRandomInteger() == 1) {
-            return "Paper";
-        }  else {
-            return "Scissors"
+            return "The computer selected" + " " + "Rock";
+        } else if (getRandomInteger() == 1) {
+            return "The computer selected" + " " + "Paper";
+        } else {
+            return "The computer selected" + " " + "Scissors"
         }
     };
-    
-
+   
    
 //If the user input is "Rock" and the computers selection is "Scissors" show "You Win!"
 //If the user input is "Rock" and the computers selection is "Paper" show "You Lose!"

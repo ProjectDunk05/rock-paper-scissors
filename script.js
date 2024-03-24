7f69b1c130a2525c88b0f85813ba32b919ba29b7
ProjectDunk05 <green.adam10@sky.com>
2024-03-24 20:39:21 +0700

    store output of player and computer functions in a variable

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 //When a user inputs "Rock", "Paper" or "Scissors"
 
     let userInput = prompt("Please enter Rock, Paper or Scissors");
-   
+
     function capitalise(str) {
         return ((str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()));
     };
 
-    function playerSelection() {
-        return "You selected" + " " + capitalise(userInput);
+    function getPlayerSelection() {
+        return capitalise(userInput);
     };
 
+    let playerSelection = getPlayerSelection();
 //Generate a random computers selection of "Rock","Paper" or "Scissors"
  
    function getRandomInteger() {
         return Math.floor(Math.random() * 3);
     };
+    
+
     function getComputerChoice() {
         if (getRandomInteger() == 0) {
-            return "The computer selected" + " " + "Rock";
+            return "Rock";
         } else if (getRandomInteger() == 1) {
-            return "The computer selected" + " " + "Paper";
+            return "Paper";
         } else {
-            return "The computer selected" + " " + "Scissors"
+            return "Scissors";
         }
     };
+
+    let resultComputer = getComputerChoice();
    
    
 //If the user input is "Rock" and the computers selection is "Scissors" show "You Win!"
+
+
+
 //If the user input is "Rock" and the computers selection is "Paper" show "You Lose!"
 //If the user input is "Rock" and the computers selection is "Rock" show "It's A Draw!"
 // Repeat the above 3 steps for user input of "Scissors" and "Paper"

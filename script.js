6fe18e016b2031f32947c0b5409b9ab0604e271b
Adam Green <green.adam10@sky.com>
2024-12-11 15:29:18 +0000

    add clickable buttons for player selection

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ let round = 0
 //When a user inputs "Rock", "Paper" or "Scissors"
 
     function getPlayerSelection() {
-        let userInput = prompt("Please enter Rock, Paper or Scissors");
-        return capitalise(userInput);
+        //let userInput = prompt("Please enter Rock, Paper or Scissors");
+       // return capitalise(userInput);
     };
 
-    function capitalise(str) {
-        return ((str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()));
-    };
+    //function capitalise(str) {
+        //return ((str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()));
+    //};
 //Generate a random computers selection of "Rock","Paper" or "Scissors"
  
    function getRandomInteger() {
@@ -82,12 +82,32 @@ console.log(`Round:${round} Player:${humanScore} Computer:${computerScore}`);
 
 // Add function to play a game that consists of 5 rounds
 
-function playGame(playerChoice, computerSelection) {
-    let resultComputer = getComputerChoice();
-    let playerSelection = getPlayerSelection();
+//function playGame(playerChoice, computerSelection) {
+   // let resultComputer = getComputerChoice();
+    //let playerSelection = getPlayerSelection();
 
-    const times = 5;
-    for(let i = 0; i < times; i++) {
-playRound(playerSelection, resultComputer);
-    };
-}
+    //const times = 5;
+    //for(let i = 0; i < times; i++) {
+//playRound(playerSelection, resultComputer);
+   // };
+//}
+
+// Click event handlers for the three buttons
+
+    let buttons = document.querySelector("#buttons");
+
+    buttons.addEventListener("click", (event) => {
+        let target = event.target;
+
+        switch(target.id) {
+            case "rock":
+                alert("Rock");
+                break;
+            case "paper":
+                 alert("Paper");
+                break;
+            case "scissors":
+                alert("Scissors");
+                break;
+        }
+    });
